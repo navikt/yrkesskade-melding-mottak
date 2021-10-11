45912bc94526adc46074e048b38e0ea6db4f47d1
Erlend Kristiansen <erlend.kristiansen@nav.no>
2021-10-11 14:31:09 +0200

    Merge conflicts

---
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/YrkesskadeMeldingMottakApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.yrkesskade.meldingmottak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YrkesskadeMeldingMottakApplication
+
+fun main(args: Array<String>) {
+	runApplication<YrkesskadeMeldingMottakApplication>(*args)
+}
---
+++ src/test/kotlin/no/nav/yrkesskade/meldingmottak/repository/SkademeldingRepositoryTest.kt
@@ -0,0 +1,38 @@
+package no.nav.yrkesskade.ysmeldingmottak.repository
+
+import no.nav.yrkesskade.meldingmottak.domain.Skademelding
+import no.nav.yrkesskade.meldingmottak.repositories.SkademeldingRepository
+import no.nav.yrkesskade.meldingmottak.testutils.docker.PostgresDockerContainer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Profile
+import java.time.Instant
+
+@DataJpaTest
+@Profile("db")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class SkademeldingRepositoryTest {
+
+    @Autowired
+    lateinit var repository: SkademeldingRepository
+
+    init {
+        PostgresDockerContainer.container
+    }
+
+    @Test
+    fun contextLoads() {
+        repository.save(
+            Skademelding(
+                null,
+                """{"some": "data"}""",
+                "me",
+                Instant.now()
+            )
+        )
+        assertThat(repository.findAll().size).isEqualTo(1)
+    }
+}
