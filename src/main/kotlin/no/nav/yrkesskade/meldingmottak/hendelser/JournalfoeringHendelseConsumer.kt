3a9fb67ad8be852720e618b83961c483590a0c9e
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2021-10-29 17:22:41 +0200

    WIP * Fjernet ack-parameter i listen() * Forsøk på test case som sender en Record på embedded kafka

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
@@ -3,7 +3,6 @@ package no.nav.yrkesskade.meldingmottak.hendelser
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
@@ -16,7 +15,7 @@ class JournalfoeringHendelseConsumer {
                    containerFactory = "kafkaListenerContainerFactory",
                    idIsGroup = false)
     @Transactional
-    fun listen(consumerRecord: ConsumerRecord<Long, JournalfoeringHendelseRecord>, ack: Acknowledgment) {
+    fun listen(consumerRecord: ConsumerRecord<Long, JournalfoeringHendelseRecord>) {
         println("${consumerRecord.value()}")
     }
 }
--- src/test/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumerTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumerTest.kt
@@ -5,10 +5,9 @@ import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroSerializer
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import no.nav.yrkesskade.meldingmottak.testutils.docker.PostgresDockerContainer
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
+import org.junit.Before
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
@@ -16,37 +15,60 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.test.condition.EmbeddedKafkaCondition
 import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.kafka.test.utils.ContainerTestUtils
 import org.springframework.test.context.ActiveProfiles
 
 
 @ActiveProfiles("integration")
 @SpringBootTest
-@EmbeddedKafka(partitions = 1, ports = [9092])
+@EmbeddedKafka(partitions = 1, ports = [9092], bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 internal class JournalfoeringHendelseConsumerTest {
 
     @Autowired
-    lateinit var journalfoeringHendelseConsumer: JournalfoeringHendelseConsumer
+    lateinit var kafkaListenerEndpointRegistry: KafkaListenerEndpointRegistry
+
+    val topicName: String = "test"
 
     @Autowired
-    lateinit var kafkaProperties: KafkaProperties
-
-    lateinit var kafkaProducer: KafkaProducer<String, JournalfoeringHendelseRecord>
+    lateinit var kafkaTemplate: KafkaTemplate<String, Any>
 
     init {
         PostgresDockerContainer.container
     }
 
+    @Before
+    fun init() {
+        for (messageListenerContainer in kafkaListenerEndpointRegistry.listenerContainers) {
+            ContainerTestUtils.waitForAssignment(messageListenerContainer, EmbeddedKafkaCondition.getBroker().partitionsPerTopic)
+        }
+    }
+
     @Test
     fun listen() {
-        kafkaProducer = KafkaProducer<String, JournalfoeringHendelseRecord>(
-                kafkaProperties.buildProducerProperties(),
-                StringSerializer(),
-                KafkaAvroSerializer()
-        )
-        kafkaProducer.send(ProducerRecord("test", JournalfoeringHendelseRecord()))
+        kafkaTemplate.send(topicName, record()).get()
+    }
+
+    fun record(): JournalfoeringHendelseRecord? {
+        return JournalfoeringHendelseRecord.newBuilder()
+                .setHendelsesId("hendelsesId")
+                .setVersjon(1)
+                .setHendelsesType("hendelsesType")
+                .setJournalpostId(1337)
+                .setJournalpostStatus("journalpostStatus")
+                .setTemaGammelt("YRK")
+                .setTemaNytt("YRK")
+                .setMottaksKanal("NRK")
+                .setKanalReferanseId("P1")
+                .setBehandlingstema("YRK")
+                .build()
     }
 
     @TestConfiguration
@@ -73,6 +95,22 @@ internal class JournalfoeringHendelseConsumerTest {
             return ConcurrentKafkaListenerContainerFactory<String, JournalfoeringHendelseRecord>().apply {
                 this.setConsumerFactory(kafkaConsumerFactory)
             }
+        }
+
+        @Bean
+        fun kafkaProducerFactory(properties: KafkaProperties,
+                                 schemaRegistryClient: MockSchemaRegistryClient): ProducerFactory<String, Any> {
+            val producerProperties = properties.buildProducerProperties()
+            return DefaultKafkaProducerFactory(
+                    producerProperties,
+                    StringSerializer(),
+                    KafkaAvroSerializer(schemaRegistryClient, producerProperties)
+            )
+        }
+
+        @Bean
+        fun kafkaTemplate(producerFactory: ProducerFactory<String, Any>): KafkaTemplate<String, Any> {
+            return KafkaTemplate(producerFactory)
         }
     }
 }
