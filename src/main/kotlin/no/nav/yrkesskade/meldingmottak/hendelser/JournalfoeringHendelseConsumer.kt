f74f4c137f76f83520fe95a67744c123a1e128b2
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2021-11-19 09:44:44 +0100

    bruker loggrammeverk

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
@@ -1,14 +1,17 @@
 package no.nav.yrkesskade.meldingmottak.hendelser
 
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
+import java.lang.invoke.MethodHandles
 import javax.transaction.Transactional
 
 
 @Service
 class JournalfoeringHendelseConsumer {
+    private val log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
 
     @KafkaListener(id = "yrkesskade-melding-mottak",
                    topics = ["\${kafka.topic.aapen-dok-journalfoering}"],
@@ -16,6 +19,6 @@ class JournalfoeringHendelseConsumer {
                    idIsGroup = false)
     @Transactional
     fun listen(@Payload record: JournalfoeringHendelseRecord) {
-        println(record)
+        log.info(record.toString())
     }
 }
