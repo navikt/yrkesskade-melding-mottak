384778c83778001a92a5aa95d01a1eb1775513d4
Erlend Kristiansen <erlend.kristiansen@nav.no>
2021-11-29 16:29:33 +0100

    Fjernet token-kode som ikke er i bruk

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/util/TokenUtil.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/util/TokenUtil.kt
@@ -2,17 +2,13 @@ package no.nav.yrkesskade.meldingmottak.util
 
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
-import no.nav.security.token.support.core.context.TokenValidationContextHolder
-import no.nav.yrkesskade.meldingmottak.clients.sts.StsClient
 
 import org.springframework.stereotype.Service
 
 @Service
 class TokenUtil(
     private val clientConfigurationProperties: ClientConfigurationProperties,
-    private val oAuth2AccessTokenService: OAuth2AccessTokenService,
-    private val tokenValidationContextHolder: TokenValidationContextHolder,
-    private val stsClient: StsClient
+    private val oAuth2AccessTokenService: OAuth2AccessTokenService
 ) {
 
     fun getAppAccessTokenWithSafScope(): String {
