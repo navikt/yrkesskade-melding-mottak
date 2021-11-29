b3d884c4ad52c1ae611aa9b1d38be7dc99986839
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2021-11-29 12:56:53 +0100

    * La til kall til SAF ved mottak av joarkhendelse * La til url til SAF i properties

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
@@ -1,7 +1,12 @@
 package no.nav.yrkesskade.meldingmottak.hendelser
 
+import com.expediagroup.graphql.client.spring.GraphQLWebClient
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
+import com.expediagroup.graphql.generated.Journalpost
+import kotlinx.coroutines.runBlocking
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
@@ -10,8 +15,10 @@ import javax.transaction.Transactional
 
 
 @Service
-class JournalfoeringHendelseConsumer {
+class JournalfoeringHendelseConsumer(@Value("\${saf.graphql.url}") private val safGraphqlUrl: String) {
     private val log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
+    val safClient = GraphQLWebClient(url = safGraphqlUrl)
 
     @KafkaListener(id = "yrkesskade-melding-mottak",
                    topics = ["\${kafka.topic.aapen-dok-journalfoering}"],
@@ -20,5 +27,12 @@ class JournalfoeringHendelseConsumer {
     @Transactional
     fun listen(@Payload record: JournalfoeringHendelseRecord) {
         log.info(record.toString())
+
+        runBlocking {
+            val response: GraphQLClientResponse<Journalpost.Result> = safClient.execute(
+                    Journalpost(variables = Journalpost.Variables(journalpostId = record.journalpostId.toString()))
+            )
+            log.info(response.data.toString())
+        }
     }
 }
