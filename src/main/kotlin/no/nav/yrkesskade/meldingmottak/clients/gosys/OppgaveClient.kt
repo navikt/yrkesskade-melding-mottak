fe54872fa56a66c839c77d2e843853ab58496ad9
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2021-12-10 11:31:33 +0100

    Logger ut respons fra Oppgave-API ved feil

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/clients/gosys/OppgaveClient.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/clients/gosys/OppgaveClient.kt
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.bodyToMono
 import java.util.UUID
 
@@ -26,20 +27,44 @@ class OppgaveClient(
     fun opprettOppgave(oppgave: OpprettJournalfoeringOppgave): Oppgave {
         val correlation = UUID.randomUUID().toString()
         log.info("Oppretter oppgave for journalpostId ${oppgave.journalpostId} med correlation $correlation")
-        return oppgaveWebClient.post()
-            .uri { uriBuilder ->
-                uriBuilder.pathSegment("api")
-                    .pathSegment("v1")
-                    .pathSegment("oppgaver")
-                    .build()
-            }
-            .contentType(MediaType.APPLICATION_JSON)
-            .header("Authorization", "Bearer ${tokenUtil.getAppAccessTokenWithOppgaveScope()}")
-            .header("X-Correlation-ID", correlation)
-            .header("Nav-Consumer-Id", applicationName)
-            .bodyValue(oppgave)
-            .retrieve()
-            .bodyToMono<Oppgave>()
-            .block() ?: throw RuntimeException("Kunne ikke lage oppgave")
+        return logTimingAndWebClientResponseException("opprettOppgave") {
+            oppgaveWebClient.post()
+                .uri { uriBuilder ->
+                    uriBuilder.pathSegment("api")
+                        .pathSegment("v1")
+                        .pathSegment("oppgaver")
+                        .build()
+                }
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer ${tokenUtil.getAppAccessTokenWithOppgaveScope()}")
+                .header("X-Correlation-ID", correlation)
+                .header("Nav-Consumer-Id", applicationName)
+                .bodyValue(oppgave)
+                .retrieve()
+                .bodyToMono<Oppgave>()
+                .block() ?: throw RuntimeException("Kunne ikke lage oppgave")
+        }
+    }
+
+    private fun <T> logTimingAndWebClientResponseException(methodName: String, function: () -> T): T {
+        val start: Long = System.currentTimeMillis()
+        try {
+            return function.invoke()
+        } catch (ex: WebClientResponseException) {
+            log.error(
+                "Got a {} error calling Oppgave {} {} with message {}",
+                ex.statusCode,
+                ex.request?.method ?: "-",
+                ex.request?.uri ?: "-",
+                ex.responseBodyAsString
+            )
+            throw ex
+        } catch (rtex: RuntimeException) {
+            log.warn("Caught RuntimeException", rtex)
+            throw rtex
+        } finally {
+            val end: Long = System.currentTimeMillis()
+            log.info("Method {} took {} millis", methodName, (end - start))
+        }
     }
 }
