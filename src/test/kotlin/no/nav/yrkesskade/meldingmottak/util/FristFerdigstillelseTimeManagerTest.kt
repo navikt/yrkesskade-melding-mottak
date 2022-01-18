eeff53ad235bd171fe8a86018d37faad47d5d607
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-01-18 12:04:12 +0100

    * Byttet ut leggTilDager med hardkodet forventet verdi * La til et par test cases

--- src/test/kotlin/no/nav/yrkesskade/meldingmottak/util/FristFerdigstillelseTimeManagerTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/meldingmottak/util/FristFerdigstillelseTimeManagerTest.kt
@@ -11,52 +11,63 @@ internal class FristFerdigstillelseTimeManagerTest {
     @Test
     fun `neste gyldige frist er neste dag om klokken er 11 paa en vanlig arbeidsdag`() {
         val klokkenElleveVanligArbeidsdag = LocalDateTime.of(2022, 1, 12, 11, 0, 0)
-        val resultat = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenElleveVanligArbeidsdag)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenElleveVanligArbeidsdag)
 
-        val forventetFrist = leggTilDager(klokkenElleveVanligArbeidsdag, 1)
-        assertThat(resultat).isEqualTo(forventetFrist)
+        val dagenEtter = LocalDate.of(2022, 1, 13)
+        assertThat(utregnetFrist).isEqualTo(dagenEtter)
     }
 
     @Test
     fun `neste gyldige frist er om to dager om klokken er 12 paa en vanlig arbeidsdag`() {
         val klokkenTolvVanligArbeidsdag = LocalDateTime.of(2022, 1, 12, 12, 0, 0)
-        val resultat = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTolvVanligArbeidsdag)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTolvVanligArbeidsdag)
 
-        val forventetFrist = leggTilDager(klokkenTolvVanligArbeidsdag, 2)
-        assertThat(resultat).isEqualTo(forventetFrist)
+        val toDagerEtter = LocalDate.of(2022, 1, 14)
+        assertThat(utregnetFrist).isEqualTo(toDagerEtter)
     }
 
     @Test
     fun `neste gyldige frist er om to dager om klokken er 13 paa en vanlig arbeidsdag`() {
         val klokkenTrettenVanligArbeidsdag = LocalDateTime.of(2022, 1, 12, 13, 0, 0)
-        val resultat = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTrettenVanligArbeidsdag)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTrettenVanligArbeidsdag)
 
-        val forventetFrist = leggTilDager(klokkenTrettenVanligArbeidsdag, 2)
-        assertThat(resultat).isEqualTo(forventetFrist)
+        val toDagerEtter = LocalDate.of(2022, 1, 14)
+        assertThat(utregnetFrist).isEqualTo(toDagerEtter)
     }
 
     @Test
     fun `neste gyldige frist er om tre dager om klokken er 11 paa en fredag`() {
         val klokkenElleveFredag = LocalDateTime.of(2022, 1, 14, 11, 0, 0)
-        val resultat = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenElleveFredag)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenElleveFredag)
 
-        val forventetFrist = leggTilDager(klokkenElleveFredag, 3)
-        assertThat(resultat).isEqualTo(forventetFrist)
+        val mandagenEtter = LocalDate.of(2022, 1, 17)
+        assertThat(utregnetFrist).isEqualTo(mandagenEtter)
+    }
+
+    @Test
+    fun `neste gyldige frist er om fire dager om klokken er 12 paa en fredag`() {
+        val klokkenTrettenFredag = LocalDateTime.of(2022, 1, 14, 12, 0, 0)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTrettenFredag)
+
+        val tirsdagenEtter = LocalDate.of(2022, 1, 18)
+        assertThat(utregnetFrist).isEqualTo(tirsdagenEtter)
     }
 
     @Test
     fun `neste gyldige frist er om fire dager om klokken er 13 paa en fredag`() {
         val klokkenTrettenFredag = LocalDateTime.of(2022, 1, 14, 13, 0, 0)
-        val resultat = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTrettenFredag)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTrettenFredag)
 
-        val forventetFrist = leggTilDager(klokkenTrettenFredag, 4)
-        assertThat(resultat).isEqualTo(forventetFrist)
+        val tirsdagenEtter = LocalDate.of(2022, 1, 18)
+        assertThat(utregnetFrist).isEqualTo(tirsdagenEtter)
     }
 
-    private fun leggTilDager(localDateTime: LocalDateTime, antallDager: Long): LocalDate {
-        return localDateTime.plusDays(antallDager)
-            .toLocalDate()
-            .atStartOfDay()
-            .toLocalDate()
+    @Test
+    fun `neste gyldige frist er om fire dager om klokken er 13 paa en torsdag`() {
+        val klokkenTrettenTorsdag = LocalDateTime.of(2022, 1, 13, 13, 0, 0)
+        val utregnetFrist = FristFerdigstillelseTimeManager.nesteGyldigeFristForFerdigstillelse(klokkenTrettenTorsdag)
+
+        val mandagenEtter = LocalDate.of(2022, 1, 17)
+        assertThat(utregnetFrist).isEqualTo(mandagenEtter)
     }
 }
