0033ab04458891861ca412960ad2bb814d36a062
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-01-25 16:35:47 +0100

    * Endringer i DDL, tatt fra ba-sak sine test-migreringer * Bruker bigserial med default sekvensering istedenfor bigint og custom sequence

--- src/main/resources/db/migration/V2__Create_tables_Task_and_Task_logg.sql
+++ src/main/resources/db/migration/V2__Create_tables_Task_and_Task_logg.sql
@@ -1,34 +1,25 @@
-CREATE TABLE task (
-    id            bigint                                               NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS task (
+    id            bigserial                                            NOT NULL PRIMARY KEY,
     payload       text                                                 NOT NULL UNIQUE,
-    status        varchar(50)  DEFAULT 'UBEHANDLET'::character varying NOT NULL,
+    status        varchar      DEFAULT 'UBEHANDLET'::character varying NOT NULL,
     versjon       bigint       DEFAULT 0,
     opprettet_tid timestamp(3) DEFAULT LOCALTIMESTAMP,
-    type          varchar(100)                                         NOT NULL,
-    metadata      varchar(4000),
+    type          varchar                                              NOT NULL,
+    metadata      varchar,
     trigger_tid   timestamp    DEFAULT LOCALTIMESTAMP,
-    avvikstype    varchar(50)
+    avvikstype    varchar
 );
 
+CREATE INDEX IF NOT EXISTS task_status_idx ON task (status);
 
-CREATE SEQUENCE task_seq INCREMENT BY 50 START WITH 1000000 NO CYCLE;
-ALTER TABLE task ALTER COLUMN id SET DEFAULT nextval('task_seq');
-ALTER SEQUENCE task_seq OWNED BY task.id;
-
-CREATE INDEX ON task (status);
-
-CREATE TABLE task_logg (
-    id            bigint       NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS task_logg (
+    id            bigserial    NOT NULL PRIMARY KEY,
     task_id       bigint       NOT NULL REFERENCES task,
-    type          varchar(15)  NOT NULL,
-    node          varchar(100) NOT NULL,
+    type          varchar  NOT NULL,
+    node          varchar NOT NULL,
     opprettet_tid timestamp(3) DEFAULT LOCALTIMESTAMP,
     melding       text,
-    endret_av     varchar(100)                                  NOT NULL
+    endret_av     varchar NOT NULL
 );
 
-CREATE SEQUENCE task_logg_seq INCREMENT BY 50 START WITH 1000000 NO CYCLE;
-ALTER TABLE task_logg ALTER COLUMN id SET DEFAULT nextval('task_logg_seq');
-ALTER SEQUENCE task_logg_seq OWNED BY task_logg.id;
-CREATE INDEX ON task_logg (task_id);
-
+CREATE INDEX IF NOT EXISTS task_logg_task_id_idx ON task_logg (task_id);
