9ed67548a80da9a52c7b4b9b5659d88e2a5cc821
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-01-26 16:41:23 +0100

    Fjernet test-endepunkt

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/controllers/SkademeldingController.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/controllers/SkademeldingController.kt
@@ -1,11 +1,7 @@
 package no.nav.yrkesskade.meldingmottak.controllers
 
-import no.nav.security.token.support.core.api.Unprotected
 import no.nav.yrkesskade.meldingmottak.models.SkademeldingDto
 import no.nav.yrkesskade.meldingmottak.services.SkademeldingService
-import no.nav.yrkesskade.meldingmottak.task.DummyTask
-import no.nav.yrkesskade.prosessering.domene.Task
-import no.nav.yrkesskade.prosessering.domene.TaskRepository
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,17 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 @RestController
 @RequestMapping(
         path = ["/api/"],
         produces = [MediaType.APPLICATION_JSON_VALUE]
 )
-class SkademeldingController(
-    private val skademeldingService: SkademeldingService,
-    private val taskRepository: TaskRepository
-) {
+class SkademeldingController(private val skademeldingService: SkademeldingService) {
 
     @PostMapping("/skademelding", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun mottaSkademelding(@RequestBody(required = true) skademeldingDto: SkademeldingDto): ResponseEntity<SkademeldingDto> {
@@ -33,13 +25,5 @@ class SkademeldingController(
     @GetMapping("/skademelding")
     fun hentSkademeldinger(): ResponseEntity<List<SkademeldingDto>> {
         return ResponseEntity.ok().body(skademeldingService.hentAlleSkademeldinger())
-    }
-
-    @Unprotected
-    @PostMapping("/dummytask")
-    fun dummytask(): Iterable<Task> {
-        val dummyTask = DummyTask.opprettTask("dette er en payload ${UUID.randomUUID()}")
-        taskRepository.save(dummyTask)
-        return taskRepository.findAll()
     }
 }
