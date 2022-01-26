36c8a2c90a7197cfc6ab377e4b3b271c9466b26e
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-01-26 16:40:46 +0100

    * Laget Task-klasse for prosessering av skanning-hendelse * Flyttet filtrering av relevante Kafka-meldinger fra consumer til service * Flyttet businesslogikk fra Service til ProsesserJournalfoertSkanningTask.kt * Service lagrer en Task per relevante Kafka-melding

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/JournalfoeringHendelseConsumer.kt
@@ -2,21 +2,16 @@ package no.nav.yrkesskade.meldingmottak.hendelser
 
 import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord
 import no.nav.yrkesskade.meldingmottak.services.JournalfoeringHendelseService
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
-import java.lang.invoke.MethodHandles
 import javax.transaction.Transactional
 
-
-private const val TEMA_YRKESSKADE = "YRK"
 
 @Service
 class JournalfoeringHendelseConsumer(
     private val journalfoeringHendelseService: JournalfoeringHendelseService
 ) {
-    private val log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
 
     @KafkaListener(
         id = "yrkesskade-melding-mottak",
@@ -26,9 +21,6 @@ class JournalfoeringHendelseConsumer(
     )
     @Transactional
     fun listen(@Payload record: JournalfoeringHendelseRecord) {
-        if (record.temaNytt.equals(TEMA_YRKESSKADE)) {
-            log.info("Mottatt journalføringhendelse: $record")
-            journalfoeringHendelseService.prosesserJournalfoeringHendelse(record)
-        }
+        journalfoeringHendelseService.prosesserJournalfoeringHendelse(record)
     }
 }
