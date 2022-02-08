799833bb693729ca7e18491aacabc7be86c9221e
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-02-08 16:47:15 +0100

    La til logging av kafkarecords hvor tema er YRK men kanal ikke er relevant

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/services/JournalfoeringHendelseService.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/services/JournalfoeringHendelseService.kt
@@ -15,24 +15,35 @@ class JournalfoeringHendelseService(private val taskRepository: TaskRepository) 
 
     private val log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
 
-    fun prosesserJournalfoeringHendelse(record: JournalfoeringHendelseRecord) {
-        if (hendelseErRelevant(record)) {
-            log.info("Mottatt journalføringhendelse: $record")
-            taskRepository.save(ProsesserJournalfoertSkanningTask.opprettTask(record.journalpostId.toString()))
-        }
-    }
-
     /**
-     * Bestemmer om en Kafka-record er relevant for prosessering hos oss.
-     * Betingelser:
+     * Tar imot en journalføringhendelse og lager en Task for opprettelse av journalføringsoppgave.
+     * Betingelser for opprettelse av task:
      * 1. Temaet må tilhøre yrkesskade (YRK)
      * 2. Mottakskanalen må være en av de vi lytter på (se enum [Kanal])
      *
      * @param record Recorden som kommer fra Kafka-topicet
      */
-    private fun hendelseErRelevant(record: JournalfoeringHendelseRecord) =
-        record.temaNytt.equals(TEMA_YRKESSKADE) &&
-                Kanal.values()
-                    .map { it.toString() }
-                    .contains(record.mottaksKanal)
+    fun prosesserJournalfoeringHendelse(record: JournalfoeringHendelseRecord) {
+        if (!record.temaNytt.equals(TEMA_YRKESSKADE)) {
+            return
+        }
+
+        if (kanalErRelevant(record)) {
+            log.info("Mottatt journalføringhendelse, oppretter journalføringsoppgave: $record")
+            taskRepository.save(ProsesserJournalfoertSkanningTask.opprettTask(record.journalpostId.toString()))
+        } else {
+            log.warn("Mottatt journalføringhendelse på tema YRK med ukjent kanal: $record")
+        }
+    }
+
+    /**
+     * Bestemmer om en Kafka-record er på relevant kanal for prosessering hos oss.
+     * Mottakskanalen må med andre ord være en av de vi lytter på (se enum [Kanal])
+     *
+     * @param record Recorden som kommer fra Kafka-topicet
+     */
+    private fun kanalErRelevant(record: JournalfoeringHendelseRecord) =
+        Kanal.values()
+            .map { it.toString() }
+            .contains(record.mottaksKanal)
 }
