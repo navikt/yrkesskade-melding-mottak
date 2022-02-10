22e51318e3365856ab76417f322e7f2d0c81b8dc
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-02-10 09:21:38 +0100

    YSMOD-53 Legger på status FEILREGISTRERT i query params

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/clients/gosys/OppgaveClient.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/clients/gosys/OppgaveClient.kt
@@ -62,6 +62,7 @@ class OppgaveClient(
                         .queryParam("status", Status.AAPNET)
                         .queryParam("status", Status.UNDER_BEHANDLING)
                         .queryParam("status", Status.FERDIGSTILT)
+                        .queryParam("status", Status.FEILREGISTRERT)
                         .build()
                 }
                 .header("Authorization", "Bearer ${tokenUtil.getAppAccessTokenWithOppgaveScope()}")
