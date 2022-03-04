7fcf3348943c6c09d7c32eb317b384621e873cf1
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-03-04 12:28:38 +0100

    YSMOD-65 flytte pdf client til WebClientConfiguration.kt

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/config/WebClientConfiguration.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/config/WebClientConfiguration.kt
@@ -11,7 +11,8 @@ import reactor.netty.http.client.HttpClient
 @Configuration
 class WebClientConfiguration(private val webClientBuilder: WebClient.Builder,
                              @Value("\${oppgave.url}") val oppgaveServiceURL: String,
-                             @Value("\${dokarkiv.url}") val dokarkivServiceURL: String) {
+                             @Value("\${dokarkiv.url}") val dokarkivServiceURL: String,
+                             @Value("\${YRKESSKADE_DOKGEN_API_URL}") val pdfServiceURL: String) {
 
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
@@ -30,6 +31,14 @@ class WebClientConfiguration(private val webClientBuilder: WebClient.Builder,
     fun dokarkivWebClient(): WebClient {
         return webClientBuilder
             .baseUrl(dokarkivServiceURL)
+            .clientConnector(ReactorClientHttpConnector(HttpClient.newConnection()))
+            .build()
+    }
+
+    @Bean
+    fun pdfWebClient(): WebClient {
+        return webClientBuilder
+            .baseUrl(pdfServiceURL)
             .clientConnector(ReactorClientHttpConnector(HttpClient.newConnection()))
             .build()
     }
