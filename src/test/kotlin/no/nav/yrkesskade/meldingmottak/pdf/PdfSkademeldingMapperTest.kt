e237b74f59f07b0b11bce7317880346ade96e0e1
Erlend Kristiansen <erlend.kristiansen@nav.no>
2022-03-08 15:30:16 +0100

    YSMOD-65 Test mapping til PdfSkademelding

--- src/test/kotlin/no/nav/yrkesskade/meldingmottak/pdf/PdfSkademeldingMapperTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/meldingmottak/pdf/PdfSkademeldingMapperTest.kt
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.yrkesskade.meldingmottak.fixtures.beriketData
 import no.nav.yrkesskade.meldingmottak.fixtures.enkelSkademeldingInnsendtHendelse
+import no.nav.yrkesskade.meldingmottak.pdf.domene.*
+import no.nav.yrkesskade.skademelding.model.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class PdfSkademeldingMapperTest {
@@ -22,10 +25,105 @@ internal class PdfSkademeldingMapperTest {
         val pdfSkademelding = PdfSkademeldingMapper.tilPdfSkademelding(record, beriketData)
         println("PdfSkademeldingen er $pdfSkademelding")
 
-        val pdfSkademeldingJson = objectMapper.writeValueAsString(pdfSkademelding)
-        println("Og JSON er $pdfSkademeldingJson")
+        assertPdfSkademelding(pdfSkademelding)
 
         val prettyJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(pdfSkademelding)
         //println("Pretty JSON er \n$prettyJson") // Kommenter inn ved behov
+    }
+
+    private fun assertPdfSkademelding(skademelding: PdfSkademelding) {
+        assertThat(skademelding).isNotNull
+        assertInnmelder(skademelding.innmelder)
+        assertSkadelidt(skademelding.skadelidt)
+        assertSkade(skademelding.skade)
+        assertHendelsesfakta(skademelding.hendelsesfakta)
+        assertDokumentInfo(skademelding.dokumentInfo)
+    }
+
+    private fun assertInnmelder(innmelder: PdfInnmelder?) {
+        assertThat(innmelder).isNotNull
+        assertThat(innmelder?.norskIdentitetsnummer?.verdi).isEqualTo("12345677777")
+        assertThat(innmelder?.navn?.verdi).isEqualTo("Inn Melder")
+        assertThat(innmelder?.paaVegneAv?.verdi).isEqualTo("123454321")
+        assertThat(innmelder?.innmelderrolle?.verdi).isEqualTo(Innmelderrolle.virksomhetsrepresentant.value)
+        assertThat(innmelder?.altinnrolleIDer?.verdi).isEqualTo(listOf("111", "22"))
+    }
+
+    private fun assertSkadelidt(skadelidt: PdfSkadelidt?) {
+        assertThat(skadelidt?.norskIdentitetsnummer?.verdi).isEqualTo("11111177777")
+        assertThat(skadelidt?.navn?.verdi).isEqualTo("Ska De Lidt")
+        assertThat(skadelidt?.bostedsadresse?.verdi).isEqualTo(
+            PdfAdresse(
+                adresselinje1 = "Stigen 7A",
+                adresselinje2 = "7730 Småby",
+                adresselinje3 = null,
+                land = ""
+            ))
+        assertThat(skadelidt?.dekningsforhold?.organisasjonsnummer?.verdi).isEqualTo("123456789")
+        assertThat(skadelidt?.dekningsforhold?.navnPaaVirksomheten?.verdi).isEqualTo("Bedriften AS")
+        assertThat(skadelidt?.dekningsforhold?.stillingstittelTilDenSkadelidte?.verdi).containsExactlyInAnyOrder(
+            Stillingstittel.altmuligmann.value,
+            Stillingstittel.agroteknikere.value
+        )
+        assertThat(skadelidt?.dekningsforhold?.rolletype?.verdi).isEqualTo(Rolletype.arbeidstaker.value)
+    }
+
+    private fun assertSkade(skade: PdfSkade?) {
+        assertThat(skade?.alvorlighetsgrad?.verdi).isEqualTo(Alvorlighetsgrad.andreLivstruendeSykdomSlashSkade.value)
+        assertThat(skade?.skadedeDeler).containsExactlyInAnyOrder(
+            PdfSkadetDel(
+                kroppsdelTabellD = Soknadsfelt("Hvor på kroppen er skaden", KroppsdelTabellD.ansikt.value),
+                skadeartTabellC = Soknadsfelt("Hva slags skade er det", SkadeartTabellC.etsing.value)
+            ),
+            PdfSkadetDel(
+                kroppsdelTabellD = Soknadsfelt("Hvor på kroppen er skaden", KroppsdelTabellD.armSlashAlbueCommaVenstre.value),
+                skadeartTabellC = Soknadsfelt("Hva slags skade er det", SkadeartTabellC.knokkelbrudd.value)
+            )
+        )
+        assertThat(skade?.antattSykefravaerTabellH?.verdi).isEqualTo(AntattSykefravaerTabellH.kjentFravRMerEnn3Dager.value)
+    }
+
+    private fun assertHendelsesfakta(hendelsesfakta: PdfHendelsesfakta?) {
+        assertThat(hendelsesfakta?.tid?.tidstype).isEqualTo(Tidstype.tidspunkt.value)
+        assertThat(hendelsesfakta?.tid?.tidspunkt?.verdi).isEqualTo(
+            PdfTidspunkt(
+                dato = "28.02.2022",
+                klokkeslett = "10.15"
+            )
+        )
+        assertThat(hendelsesfakta?.tid?.periode).isEqualTo(Soknadsfelt(
+            "Når skjedde ulykken som skal meldes?",
+            PdfPeriode("", "")
+        ))
+        assertThat(hendelsesfakta?.tid?.ukjent).isFalse
+        assertThat(hendelsesfakta?.naarSkjeddeUlykken?.verdi).isEqualTo(NaarSkjeddeUlykken.iAvtaltArbeidstid.value)
+        assertThat(hendelsesfakta?.hvorSkjeddeUlykken?.verdi).isEqualTo(HvorSkjeddeUlykken.pArbeidsstedetUte.value)
+        assertThat(hendelsesfakta?.ulykkessted?.sammeSomVirksomhetensAdresse?.verdi).isEqualTo("Ja")
+        assertThat(hendelsesfakta?.ulykkessted?.adresse?.verdi).isEqualTo(
+            PdfAdresse(
+                adresselinje1 = "Storgaten 13",
+                adresselinje2 = "2345 Småbygda",
+                adresselinje3 = null,
+                land = "SVERIGE"
+            )
+        )
+        assertThat(hendelsesfakta?.aarsakUlykkeTabellAogE?.verdi).containsExactlyInAnyOrder(
+            UlykkesAarsakTabellAogE.kjemikalier.value,
+            UlykkesAarsakTabellAogE.fallAvPerson.value
+        )
+        assertThat(hendelsesfakta?.bakgrunnsaarsakTabellBogG?.verdi).containsExactlyInAnyOrder(
+            BakgrunnsaarsakTabellBogG.defektUtstyr.value,
+            BakgrunnsaarsakTabellBogG.feilPlassering.value,
+            BakgrunnsaarsakTabellBogG.mangelfullOpplRing.value
+        )
+        assertThat(hendelsesfakta?.stedsbeskrivelseTabellF?.verdi).isEqualTo(StedsbeskrivelseTabellF.plassForIndustriellVirksomhet.value)
+        assertThat(hendelsesfakta?.utfyllendeBeskrivelse?.verdi).contains("blabla bla ")
+    }
+
+    private fun assertDokumentInfo(dokumentInfo: PdfDokumentInfo) {
+        assertThat(dokumentInfo.dokumentnavn).isEqualTo("Melding om yrkesskade eller yrkessykdom")
+        assertThat(dokumentInfo.dokumentnummer).isEqualTo("NAV 13")
+        assertThat(dokumentInfo.dokumentDatoPrefix).isEqualTo("Innsendt digitalt ")
+        assertThat(dokumentInfo.dokumentDato).isEqualTo("28.02.2022")
     }
 }
