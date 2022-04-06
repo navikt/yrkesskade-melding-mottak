2077b2a7542419b0ba3ee4793f871d99f718f776
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-04-06 09:04:12 +0200

    Legger til nedlagt enhet

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/util/extensions/JournalpostExt.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/util/extensions/JournalpostExt.kt
@@ -18,7 +18,7 @@ fun Journalpost.journalfoerendeEnhetEllerNull(): String? {
         return null
     }
 
-    val nedlagteEnheter = listOf("0889", "4203", "0289")
+    val nedlagteEnheter = listOf("0889", "4203", "0289", "1664")
     if (nedlagteEnheter.contains(journalfoerendeEnhet)) {
         return null
     }
