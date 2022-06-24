840e78c6871eafa0d0056647b5b937691189820d
Erlend Kristiansen <erlend.kristiansen@nav.no>
2022-06-24 16:09:53 +0200

    YSMOD-307 Fiks IT, endre til det nye topic-navnet

--- src/test/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/SkademeldingInnsendtHendelseConsumerIT.kt
+++ src/test/kotlin/no/nav/yrkesskade/meldingmottak/hendelser/SkademeldingInnsendtHendelseConsumerIT.kt
@@ -19,7 +19,7 @@ import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.kafka.test.utils.ContainerTestUtils
 
-private const val TOPIC = "yrkesskade.privat-yrkesskade-skademeldinginnsendt"
+private const val TOPIC = "yrkesskade.privat-yrkesskade-skademeldinginnsendt-v2"
 
 private const val NUM_BROKERS = 1
 
