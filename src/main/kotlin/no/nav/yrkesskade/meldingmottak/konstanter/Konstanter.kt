1c06e930394de8b42d7146e0fca920d8c411cb8b
Erlend Kristiansen <erlend.kristiansen@nav.no>
2022-07-06 10:44:02 +0200

    YSMOD-445 Konstanter i egen fil

---
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/konstanter/Konstanter.kt
@@ -0,0 +1,9 @@
+package no.nav.yrkesskade.meldingmottak.konstanter
+
+const val BREVKODE_DIGITAL_SKADEFORKLARING = "NAV 13-00.21"
+const val BREVKODE_TANNLEGEERKLAERING = "NAV 13-00.08"
+
+const val TEMA_YRKESSKADE = "YRK"
+
+const val TITTEL_DIGITAL_SKADEFORKLARING_ARKIV = "Arkivlogg fra innsending"
+const val TITTEL_DIGITAL_SKADEFORKLARING = "Skadeforklaring ved arbeidsulykke"
