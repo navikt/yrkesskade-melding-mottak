7887569ee673aa6ea923f54d0a3d106d33566d27
Erlend Kristiansen <erlend.kristiansen@nav.no>
2022-08-08 12:53:03 +0200

    chore Merge-konflikter

--- src/test/kotlin/no/nav/yrkesskade/meldingmottak/task/ProsesserJournalfoeringHendelseTaskMockTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/meldingmottak/task/ProsesserJournalfoeringHendelseTaskMockTest.kt
@@ -9,16 +9,7 @@ import no.nav.yrkesskade.meldingmottak.clients.bigquery.BigQueryClientStub
 import no.nav.yrkesskade.meldingmottak.clients.gosys.OppgaveClient
 import no.nav.yrkesskade.meldingmottak.clients.graphql.PdlClient
 import no.nav.yrkesskade.meldingmottak.clients.graphql.SafClient
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultMedBrevkodeTannlegeerklaering
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultMedJournalposttypeUtgaaende
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultMedJournalstatusFeilregistrert
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultMedTemaSYK
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultMedUgyldigBrukerIdType
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultUtenBruker
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultUtenBrukerId
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultUtenDokumenter
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultWithBrukerAktoerid
-import no.nav.yrkesskade.meldingmottak.fixtures.journalpostResultWithBrukerFnr
+import no.nav.yrkesskade.meldingmottak.fixtures.*
 import no.nav.yrkesskade.meldingmottak.hendelser.DokumentTilSaksbehandlingClient
 import no.nav.yrkesskade.meldingmottak.services.RutingService
 import org.assertj.core.api.Assertions.assertThat
