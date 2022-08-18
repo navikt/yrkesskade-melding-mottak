8bba93669dcd5ae1c462898220c246b5466a06f8
Erlend Kristiansen <erlend.kristiansen@nav.no>
2022-08-18 18:29:36 +0200

    YSMOD-374 Logg kall til ys-infotrygd

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/config/WebClientConfiguration.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/config/WebClientConfiguration.kt
@@ -1,12 +1,18 @@
 package no.nav.yrkesskade.meldingmottak.config
 
+import no.nav.yrkesskade.meldingmottak.util.getSecureLogger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
+import java.util.function.Consumer
+
 
 @Configuration
 class WebClientConfiguration(private val webClientBuilder: WebClient.Builder,
@@ -20,6 +26,7 @@ class WebClientConfiguration(private val webClientBuilder: WebClient.Builder,
     companion object {
         @Suppress("JAVA_CLASS_ON_COMPANION")
         private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+        private val secureLogger = getSecureLogger()
     }
 
     @Bean
@@ -67,6 +74,27 @@ class WebClientConfiguration(private val webClientBuilder: WebClient.Builder,
         return webClientBuilder
             .baseUrl(infotrygdServiceURL)
             .clientConnector(ReactorClientHttpConnector(HttpClient.newConnection()))
+            .filter(logRequest())
             .build()
     }
+
+    // This method returns filter function which will log request data
+    private fun logRequest(): ExchangeFilterFunction {
+        return ExchangeFilterFunction.ofRequestProcessor { clientRequest: ClientRequest ->
+            secureLogger.info("Request: {} {}", clientRequest.method(), clientRequest.url())
+            clientRequest.headers()
+                .forEach { name: String?, values: List<String?> ->
+                    values.forEach(
+                        Consumer { value: String? ->
+                            secureLogger.info(
+                                "{}={}",
+                                name,
+                                value
+                            )
+                        })
+                }
+            Mono.just(clientRequest)
+        }
+    }
+
 }
