099cd25e2346a27f02ca74b62eedfcc79ffbae0a
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-09-27 12:53:33 +0200

    YSMOD-598 Midlertidig: Skrur av ruting av arbeidstilsynsmelding til yrkessykdomsenheten; trenger PDL-oppslag

--- src/main/kotlin/no/nav/yrkesskade/meldingmottak/util/extensions/JournalpostExt.kt
+++ src/main/kotlin/no/nav/yrkesskade/meldingmottak/util/extensions/JournalpostExt.kt
@@ -2,8 +2,6 @@ package no.nav.yrkesskade.meldingmottak.util.extensions
 
 import com.expediagroup.graphql.generated.journalpost.DokumentInfo
 import com.expediagroup.graphql.generated.journalpost.Journalpost
-import no.nav.yrkesskade.meldingmottak.domene.Brevkode
-import no.nav.yrkesskade.meldingmottak.konstanter.ENHET_YRKESSYKDOM
 
 fun Journalpost.hentHovedDokument(): DokumentInfo? = dokumenter?.firstOrNull { it!!.brevkode != null }
 fun Journalpost.hentHovedDokumentTittel(): String = hentHovedDokument()?.tittel.orEmpty()
@@ -20,7 +18,6 @@ fun Journalpost.journalfoerendeEnhetEllerNull(): String? {
     val nedlagteEnheter = listOf("0889", "4203", "0289", "2089", "1664", "0189", "1789", "4417", "0389", "1688", "1812")
 
     return when {
-        hentBrevkode() == Brevkode.ARBEIDSTILSYNSMELDING_KOPI.kode -> ENHET_YRKESSYKDOM
         nedlagteEnheter.contains(journalfoerendeEnhet) -> null
         else -> journalfoerendeEnhet
     }
